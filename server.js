d951ce6ac0b4c96078c124993341e5f8b062fff3
CreateAqwa <123432578+CreateAqwa@users.noreply.github.com>
2026-07-18 12:17:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const app = express();
 
 // Middlewares
 app.use(express.json());   // JSON data read karne ke liye (axios ke liye zaroori)
-app.use(cors());           // taaki HTML file se request block na ho
+// app.use(cors());           // taaki HTML file se request block na ho
 
+
+app.use(cors({
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
+}));
 // ================== MongoDB Connect ==================
 // Apni Atlas connection string yaha daalo
 // mongoose.connect("mongodb+srv://USER_DATABASE_syED:NyRvELuIzA36do1U@cluster0.z3w8chw.mongodb.net/?appName=Cluster0")
@@ -15,13 +24,13 @@ app.use(cors());           // taaki HTML file se request block na ho
 //   .catch(err => console.log("Connection error:", err));
 
 
-// if call via url key and Value
+// if call via url key and Value (online site ka lea key value)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected!"))
   .catch(err => console.log("Connection error:", err));
 
-  
+
 // ================== Schema & Model ==================
 const notesSchema = new mongoose.Schema({
   title: String,
